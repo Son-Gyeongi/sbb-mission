ca9b87051369bf88375942af72ac1bdf39617c4f
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-21 17:28:57 +0900

    2-1, springboot project structure

--- src/main/java/com/ll/sbbmission/SbbMissionApplication.java
+++ src/main/java/com/ll/sbbmission/SbbMissionApplication.java
@@ -3,8 +3,9 @@ package com.ll.sbbmission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트의 모든 설정이 관리
 public class SbbMissionApplication {
+    // 모든 프로그램에는 시작을 담당하는 파일
 
     public static void main(String[] args) {
         SpringApplication.run(SbbMissionApplication.class, args);
