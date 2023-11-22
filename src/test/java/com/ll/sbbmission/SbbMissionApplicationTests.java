dc930b7ffe5c3b92b27dd2dd8453e54796fa9d7b
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 15:18:48 +0900

    2-5, answer / jpa save()

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ll.sbbmission;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest // 스프링부트 테스트 클래스임을 의미
 class SbbMissionApplicationTests {
@@ -15,13 +14,19 @@ class SbbMissionApplicationTests {
     @Autowired // 스프링의 DI 기능 : 스프링이 객체를 대신 생성하여 주입
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test // 테스트 메서드임을 나타낸다.
     void testJpa() {
-        assertEquals(2, this.questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        Assertions.assertTrue(oq.isPresent());
         Question q = oq.get();
-        this.questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q); // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
     }
 }
