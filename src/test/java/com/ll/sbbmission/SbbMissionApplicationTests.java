8f590c1aa85dd71155a8b7025ecd88fda08800e8
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 15:13:31 +0900

    2-5, jpa delete()

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,11 +1,13 @@
 package com.ll.sbbmission;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest // 스프링부트 테스트 클래스임을 의미
 class SbbMissionApplicationTests {
@@ -15,10 +17,11 @@ class SbbMissionApplicationTests {
 
     @Test // 테스트 메서드임을 나타낸다.
     void testJpa() {
+        assertEquals(2, this.questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
-        Assertions.assertTrue(oq.isPresent());
+        assertTrue(oq.isPresent());
         Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 }
