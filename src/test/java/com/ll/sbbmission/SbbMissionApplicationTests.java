2440818199494367bf28be88aa0023cea4356bd5
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 14:33:04 +0900

    2-5, jpa save()

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ll.sbbmission;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+
+@SpringBootTest // 스프링부트 테스트 클래스임을 의미
 class SbbMissionApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired // 스프링의 DI 기능 : 스프링이 객체를 대신 생성하여 주입
+    private QuestionRepository questionRepository;
+
+    @Test // 테스트 메서드임을 나타낸다.
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1); // 첫번째 질문 저장
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2); // 두번째 질문 저장
     }
 
 }
