0511214113793dcf1a6bb16751161f60a2e99611
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-27 01:33:02 +0900

    2-10, question detail controller/service/repository

--- src/main/java/com/ll/sbbmission/question/QuestionController.java
+++ src/main/java/com/ll/sbbmission/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,10 +14,19 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    // 게시글 모두 불러오기
     @GetMapping("/question/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    // 질문 상세 페이지
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/sbbmission/question/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.ll.sbbmission.question;
 
+import com.ll.sbbmission.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +13,19 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    // 게시글 모두 불러오기
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    // 질문 상세 페이지 - 1개 조회
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
