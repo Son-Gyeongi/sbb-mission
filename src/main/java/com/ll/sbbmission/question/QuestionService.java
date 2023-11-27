3763a793eb3ed4b49fad97171177052d6454a6aa
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-28 03:38:01 +0900

    3-2, paging move, list DESC

--- src/main/java/com/ll/sbbmission/question/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/QuestionService.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -18,7 +21,10 @@ public class QuestionService {
 
     // 게시글 모두 불러오기
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        // 작성일시 역순으로 조회하기
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate")); // 기준 createDate
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 
