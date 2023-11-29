ecc16ff3ac9ae6b4dca8d19506d021246d2f9cb8
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-30 00:18:00 +0900

    3-10, answer modify

--- src/main/java/com/ll/sbbmission/answer/AnswerController.java
+++ src/main/java/com/ll/sbbmission/answer/AnswerController.java
@@ -6,13 +6,16 @@ import com.ll.sbbmission.user.SiteUser;
 import com.ll.sbbmission.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -46,5 +49,42 @@ public class AnswerController {
         // 답변을 저장한다.
         this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    // 답변 수정 GET
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id,
+                               Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+        // 답변 수정시 기존의 내용이 필요하므로 AnswerForm 객체에 조회한 값을 저장
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    // 답변 수정 POST
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+
+        // 답변 찾기
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+        this.answerService.modify(answer, answerForm.getContent());
+        // 답변 수정을 완료한 후에는 질문 상세 페이지로 돌아가기 위해 answer.getQuestion.getId()로 질문의 아이디를 가져왔다.
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
